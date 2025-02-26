db9bfe7b293d90c153fe4695600d4579fb593b95
MakoDoli <makodolidze@hotmail.com>
2025-02-26 14:58:17 +0400

    add signIn and signOut server actions

--- app/_lib/auth.ts
+++ app/_lib/auth.ts
@@ -1,5 +1,15 @@
 import NextAuth from "next-auth";
 import Google from "next-auth/providers/google";
+import { createGuest, getGuest } from "./data-service";
+
+declare module "next-auth" {
+  interface User {
+    guestId?: string;
+  }
+  interface Session {
+    user: User;
+  }
+}
 
 export const {
   handlers: { GET, POST },
@@ -13,4 +23,27 @@ export const {
       clientSecret: process.env.AUTH_GOOGLE_SECRET,
     }),
   ],
+  callbacks: {
+    authorized({ auth }) {
+      return !!auth?.user;
+    },
+    async signIn({ user }) {
+      try {
+        const existingGuest = await getGuest(user.email!);
+        if (!existingGuest)
+          await createGuest({ email: user.email, fullName: user.name });
+        return true;
+      } catch {
+        return false;
+      }
+    },
+    async session({ session, user }) {
+      const guest = await getGuest(session.user.email);
+      session.user.guestId = guest.id;
+      return session;
+    },
+  },
+  pages: {
+    signIn: "/login",
+  },
 });
--- app/account/page.tsx
+++ app/account/page.tsx
@@ -1,14 +1,18 @@
 import { Metadata } from "next";
 import React from "react";
+import { auth } from "../_lib/auth";
 
 export const metadata: Metadata = {
   title: "Guest area",
 };
 
-export default function Page() {
+export default async function Page() {
+  const session = await auth();
+  const firstName = session?.user?.name?.split(" ")[0];
+
   return (
     <h2 className="font-semibold text-2xl text-accent-400 mb-7">
-      Welcome, guest
+      Welcome, {firstName}
     </h2>
   );
 }
